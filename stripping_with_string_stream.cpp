972c85ba271619d5d457eaa2cd7acc6632b9bf1d
Micdroid <micdroid622@gmail.com>
2024-02-07 19:20:22 +0530

    Stripping with String stream

---
+++ stripping_with_string_stream.cpp
@@ -0,0 +1,29 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    int val;
+    char discard;
+    vector<int> v;
+    while (ss >> val)
+    {
+        v.push_back(val);
+        ss >> discard;
+    }
+    return v;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
